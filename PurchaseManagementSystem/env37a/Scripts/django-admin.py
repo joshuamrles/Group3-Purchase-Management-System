86f521d9d7874331b9b731917a44d82fcc583617
Raden Ameer <radenameerh@gmail.com>
2020-09-18 17:23:05 +0800

    ui changes for delivery order

---
+++ PurchaseManagementSystem/env37a/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\ppapp\SEM_PROJECTG3\PurchaseManagementSystem\env37a\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
